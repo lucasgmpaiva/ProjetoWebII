5bb51f40fddf15beef524f21d0d88c19aa3bff57
lucasgmpaiva <lucasgmpaiva1@gmail.com>
2021-03-04 21:27:33 -0300

    Adicionando listagem das buscas

--- projetoweb2/src/main/java/com/projeto/web/control/IndexController.java
+++ projetoweb2/src/main/java/com/projeto/web/control/IndexController.java
@@ -1,10 +1,13 @@
 package com.projeto.web.control;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,59 +32,71 @@ public class IndexController {
 	 
 	
 	@GetMapping("/movie/popular")
-	public String getPopularMovies() {
+	public String getPopularMovies(Model model) {
 		MovieSet popularMovies = consumeAPI.findMostPopularMovies();
+		model.addAttribute("movies", popularMovies.getResults());
 		logger.info(Arrays.asList(popularMovies.getResults()).toString());
-		return "index";
+		return "list_movies";
 	}
 	
 	  @GetMapping("/series/popular")
-	  public String getPopularSeries() {
+	  public String getPopularSeries(Model model) {
 		  TVSeriesSet popularSeries = consumeAPI.findMostPopularSeries();
+		  model.addAttribute("tvseries", popularSeries.getResults());
 		  logger.info(Arrays.asList(popularSeries.getResults()).toString());
-		  return "index";
+		  return "list_series";
 	  }
 	  
 	  @GetMapping("/movie/top")
-	  public String getTopRatedMovies() {
+	  public String getTopRatedMovies(Model model) {
 		  MovieSet topRatedMovies = consumeAPI.findTopRatedMovies();
+		  model.addAttribute("movies", topRatedMovies.getResults());
 		  logger.info(Arrays.asList(topRatedMovies.getResults()).toString());
-		  return "index";
+		  return "list_movies";
 	  }
 	  
 	  @GetMapping("/series/top")
-	  public String getTopRatedSeries() {
+	  public String getTopRatedSeries(Model model) {
 		  TVSeriesSet topRatedSeries = consumeAPI.findTopRatedSeries();
+		  model.addAttribute("tvseries", topRatedSeries.getResults());
 		  logger.info(Arrays.asList(topRatedSeries.getResults()).toString());
-		  return "index";
+		  return "list_series";
 	  }
 	  
 	  @GetMapping("/movie/{id}")
-	  public String getMovieById(@PathVariable(value = "id") Long id) {
+	  public String getMovieById(@PathVariable(value = "id") Long id, Model model) {
 		  Movie movie = consumeAPI.findMovieById(id);
+		  List<Movie> movieArray = new ArrayList<Movie>();
+		  movieArray.add(movie);
+		  model.addAttribute("movies", movieArray);
 		  logger.info(movie.toString());
-		  return "index";
+		  return "list_movies";
 	  }
 	  
 	  @GetMapping("/movie/search/{query}")
-	  public String getMovieBySearch(@PathVariable(value = "query") String query) {
+	  public String getMovieBySearch(@PathVariable(value = "query") String query, Model model) {
 		  MovieSet movies = consumeAPI.findMovieBySearch(query);
+		  model.addAttribute("movies", movies.getResults());
 		  logger.info(Arrays.asList(movies.getResults()).toString());
-		  return "index";
+		  return "list_movies";
 	  }
 	  
-	  @GetMapping("/tv/{id}")
-	  public String getSeriesById(@PathVariable(value = "id") Long id) {
+	  @GetMapping("/series/{id}")
+	  public String getSeriesById(@PathVariable(value = "id") Long id, Model model) {
 		  TVSeries tvSeries = consumeAPI.findSeriesById(id);
+		  List<TVSeries> tvSeriesArray = new ArrayList<TVSeries>();
+		  tvSeriesArray.add(tvSeries);
+		  model.addAttribute("tvseries", tvSeriesArray);
 		  logger.info(tvSeries.toString());
-		  return "index";
+		  return "list_series";
 	  }
 	  
-	  @GetMapping("/tv/search/{query}")
-	  public String getSeriesBySearch(@PathVariable(value = "query") String query) {
+	  @GetMapping("/series/search/{query}")
+	  public String getSeriesBySearch(@PathVariable(value = "query") String query, Model model) {
 		  TVSeriesSet series = consumeAPI.findSeriesBySearch(query);
+		  model.addAttribute("tvseries", series.getResults());
 		  logger.info(Arrays.asList(series.getResults()).toString());
-		  return "index";
+		  return "list_series";
 	  }
 	 
 
